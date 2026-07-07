34b59d8f47b0754352fcae37deab60ff2d3f1dd4
Apurva Srivastava <apurvasrivastava2403@gmail.com>
2026-07-07 12:34:46 +0530

    ✅ Phase 8G Resend OTP

--- url-shortener/src/routes/auth.routes.js
+++ url-shortener/src/routes/auth.routes.js
@@ -6,17 +6,12 @@ const {
   registerSchema,
   loginSchema,
   verifyEmailSchema,
+  resendOTPSchema,
 } = require("../validators/auth.validator");
 
 router.post("/register", validate(registerSchema), authController.register);
 router.post("/login", validate(loginSchema), authController.login);
-router.post("/verify-email", validate(verifyEmailSchema), authController.verifyEmail);
+router.post("/verify-email",validate(verifyEmailSchema),authController.verifyEmail);
 // router.post("/test-email", authController.testEmail);
-router.post(
-  "/resend-otp",
-
-  validate(resendOTPSchema),
-
-  authController.resendOTP,
-);
+router.post("/resend-otp", validate(resendOTPSchema), authController.resendOTP);
 module.exports = router;
