33f274d39ae323c2253d84dc663edb8adba32d46
Apurva Srivastava <apurvasrivastava2403@gmail.com>
2026-07-28 12:47:06 +0530

    testing email error

--- url-shortener/src/config/mail.js
+++ url-shortener/src/config/mail.js
@@ -2,11 +2,17 @@ const nodemailer = require("nodemailer");
 const env = require("../config/env");
 const logger = require("../utils/logger");
 
+logger.info({
+  host: env.SMTP_HOST,
+  port: env.SMTP_PORT,
+  smtpEmail: env.SMTP_EMAIL,
+});
+
 const transporter = nodemailer.createTransport({
   host: env.SMTP_HOST,
   port: Number(env.SMTP_PORT),
   secure: false, // STARTTLS on port 587
-
+  requireTLS: true,
   auth: {
     user: env.SMTP_EMAIL,
     pass: env.SMTP_PASSWORD,
